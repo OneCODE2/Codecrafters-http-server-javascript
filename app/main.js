319eda88ceb5afde5e9b08ddd904122d17f75aa8
OneCODE2 <adityagajbhiye2503@gmail.com>
2024-09-03 03:29:56 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ const server = net.createServer((socket) => {
           
             const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
             socket.write(httpResponse);
-        } else if(url.includes("/echo/")){
+        }else if(url.includes("/echo/")){
             const content = url.split('/echo/')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
         }
