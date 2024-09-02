641ec9f1180d3971bbf151d4d1f585d6adaf1504
OneCODE2 <adityagajbhiye2503@gmail.com>
2024-09-03 03:11:22 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,24 +1,3 @@
-// const net = require("net");
-
-// // You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
-
-// // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//     socket.on("data",(buffer)=>{
-//      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-//      socket.end();
-//     });
-//     socket.on("close", () => {
-//       socket.end();
-//       server.close();
-//   });
-// });
-
-
-// server.listen(4221, "localhost");
-
-
 
 const net = require('net');
 
@@ -35,7 +14,7 @@ const server = net.createServer((socket) => {
             const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
             socket.write(httpResponse);
         } else {
-          const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
+          const httpResponse = 'HTTP/1.1 404 Not Found\r\n\r\n';
             socket.write(httpResponse);
         }
         socket.end();
